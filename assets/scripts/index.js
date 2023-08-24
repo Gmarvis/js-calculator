683891a6e93f9ee3cbdd7c64d2356be73255fa83
gmarvis <sgmarvis@gmail.com>
2023-08-24 08:09:58 +0100

    js and css linters

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,24 +1,22 @@
-let result = document.getElementById('inputext');
+const result = document.getElementById('inputext')
 
 // get numbers and operators from input field
-let calculate = (number)=>{
-    result.value += number
+const calculate = (number) => {
+  result.value += number
 }
 
-let Result = ()=>{
-    try{
-
-        result.value = eval(result.value)// the eval() js funtion takes A String value that contains valid JavaScript code. Evaluates JavaScript code and executes it.
-    }
-    catch(err){
-        alert('Invalid input')
-    }
+const Result = () => {
+  try {
+    result.value = eval(result.value)// the eval() js funtion takes A String value that contains valid JavaScript code. Evaluates JavaScript code and executes it.
+  } catch (err) {
+    alert('Invalid input')
+  }
 }
 
-function clr(){
-    result.value= " "
+function clr () {
+  result.value = ' '
 }
 
-function del(){
-    result.value=result.value.slice(0,-1);
+function del () {
+  result.value = result.value.slice(0, -1)
 }
